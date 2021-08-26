2b72fdde3b099e0bc15ff8f901af57cbe0cc5aea
Dennis Roth <rothdennis92@gmail.com>
2021-08-26 21:10:09 +0200

    add Product Information template

--- cisco_support/product_information.py
+++ cisco_support/product_information.py
@@ -2,3 +2,12 @@ class PI:
 
     def __init__(self) -> None:
         pass
+
+    def getBySerialNumbers(self) -> dict:
+        pass
+
+    def getByProductIDs(self) -> dict:
+        pass
+
+    def getMDFInformationByProductIDs(self) -> dict:
+        pass
