3cb2995312e5a8ea78865c3a9a1493d24577a4e9
Dennis Roth <rothdennis92@gmail.com>
2021-08-26 17:00:49 +0200

    impmement eox by dates

--- cisco_support/eox.py
+++ cisco_support/eox.py
@@ -1,10 +1,44 @@
+import requests
+
 class EoX:
 
-    def __init__(self, key, secret) -> None:
-        pass
+    __headers = None
 
-    def getByDates(self):
-        pass
+    def __init__(self, key, secret):
+
+        token = self.__getToken(key, secret)      
+
+        self.__headers = {
+            'Authorization': f'Bearer {token}',
+            'Accept': 'application/json'
+        }
+
+    def __getToken(self, client_id, client_secret) -> str:
+
+        url = 'https://cloudsso.cisco.com/as/token.oauth2'
+
+        params = {
+            'grant_type': 'client_credentials',
+            'client_id': client_id,
+            'client_secret': client_secret
+        }
+
+        headers = {
+            'Content-Type':'application/x-www-form-urlencoded'
+        }
+
+        r = requests.post(url=url, params=params, headers=headers)
+
+        return r.json()['access_token']
+
+
+    def getByDates(self, startDate, endDate, pageIndex):
+        
+        url = f'https://api.cisco.com/supporttools/eox/rest/5/EOXByDates/{pageIndex}/{startDate}/{endDate}'
+
+        r = requests.get(url=url, headers=self.__headers)
+
+        return r.json()
 
     def getByProducsIDs(self):
         pass
--- test.py
+++ test.py
@@ -7,6 +7,7 @@ from cisco_support.serial_number_information import SNI
 from cisco_support.service_order_return import RMA
 from cisco_support.software_suggestions import SS
 
+import json
 from configparser import ConfigParser
 config = ConfigParser()
 config.read('config.ini')
@@ -14,8 +15,14 @@ config.read('config.ini')
 client_key = config['cisco_support']['client_key']
 client_secret = config['cisco_support']['client_secret']
 
+# Testing EoX
 eox = EoX(client_key, client_secret)
-eox.getByDates()
-eox.getByProducsIDs()
-eox.getBySerialNumbers()
-eox.getBySoftwareReleaseStrings()
+
+a = eox.getByDates('2021-01-31','2021-03-31',1)
+print(json.dumps(a, indent=4))
+
+#eox.getByProducsIDs()
+#eox.getBySerialNumbers()
+#eox.getBySoftwareReleaseStrings()
+
+# Testing ...
