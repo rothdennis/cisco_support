4751f892f05c35d3fb6d1e97cb04ba0d9b49319c
Dennis Roth <rothdennis92@gmail.com>
2021-08-26 21:06:57 +0200

    add Case template

--- cisco_support/case.py
+++ cisco_support/case.py
@@ -2,3 +2,15 @@ class Case:
 
     def __init__(self) -> None:
         pass
+
+    def getCaseSummary(self) -> dict:
+        pass
+
+    def getCaseDetails(self) -> dict:
+        pass
+
+    def getByContractID(self) -> dict:
+        pass
+
+    def getByUserID(self) -> dict:
+        pass
