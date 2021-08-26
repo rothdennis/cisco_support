5d811ea89d649f6413a6fd4e794d667338de0f05
Dennis Roth <rothdennis92@gmail.com>
2021-08-26 21:15:35 +0200

    add Serial Number to Information template

--- cisco_support/serial_number_information.py
+++ cisco_support/serial_number_information.py
@@ -2,3 +2,18 @@ class SNI:
 
     def __init__(self) -> None:
         pass
+
+    def getCoverageStatusBySerialNumbers(self) -> dict:
+        pass
+
+    def getCoverageSummaryBySerialNumbers(self) -> dict:
+        pass
+
+    def getCoverageSummaryByInstanceNumbers(self) -> dict:
+        pass
+
+    def getOrderableProductIDsBySerialNumbers(self) -> dict:
+        pass
+
+    def getOwnerCoverageStatusBySerialNumbers(self) -> dict:
+        pass
