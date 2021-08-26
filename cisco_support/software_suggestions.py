3e4c3f5716e2ac8b6860c2b20c52336ee3019524
Dennis Roth <rothdennis92@gmail.com>
2021-08-26 21:21:17 +0200

    add Software Suggestion template

--- cisco_support/software_suggestions.py
+++ cisco_support/software_suggestions.py
@@ -2,3 +2,21 @@ class SS:
 
     def __init__(self) -> None:
         pass
+
+    def getSuggestedReleasesAndImagesByProductIDs(self) -> dict:
+        pass
+
+    def getSuggestedReleasesByProductIDs(self) -> dict:
+        pass
+
+    def getCompatibleAndSuggestedSoftwareReleasesByProductID(self) -> dict:
+        pass
+
+    def getSuggestedReleasesAndImagesByMDFIDs(self) -> dict:
+        pass
+
+    def getSuggestedReleasesByMDFIDs(self) -> dict:
+        pass
+
+    def getCompatibleAndSuggestedSoftwareReleasesByMDFID(self) -> dict:
+        pass
