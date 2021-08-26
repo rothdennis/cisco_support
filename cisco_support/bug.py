5189abe493840a8b85b208ded9cc9c308e25f46c
Dennis Roth <rothdennis92@gmail.com>
2021-08-26 21:00:39 +0200

    add bug template

--- cisco_support/bug.py
+++ cisco_support/bug.py
@@ -2,3 +2,27 @@ class Bug:
 
     def __init__(self) -> None:
         pass
+
+    def getByIDs(self) -> dict:
+        pass
+
+    def getByBasePoductIDs(self) -> dict:
+        pass
+
+    def getByBaseProductIDsAndSoftwareReleases(self) -> dict:
+        pass
+
+    def getByKeywords(self) -> dict:
+        pass
+
+    def getByProductSeriesAndAffectedSoftwareRelease(self) -> dict:
+        pass
+
+    def getByProductSeriesAndFixedInSoftwareRelease(self) -> dict:
+        pass
+
+    def getByProductNameAndAffectedSoftwareRelease(self) -> dict:
+        pass
+
+    def getByProductNameAndFixedInSoftwareRelease(self) -> dict:
+        pass
