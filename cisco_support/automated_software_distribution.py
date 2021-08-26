b978b45aed0951e6f95e119848c7e526134d6725
Dennis Roth <rothdennis92@gmail.com>
2021-08-26 21:04:19 +0200

    add Automated Software Distribution template

--- cisco_support/automated_software_distribution.py
+++ cisco_support/automated_software_distribution.py
@@ -2,3 +2,9 @@ class ASD:
 
     def __init__(self) -> None:
         pass
+
+    def getByProductIDAndRelease(self) -> dict:
+        pass
+
+    def getByProductIDAndImage(self) -> dict:
+        pass
