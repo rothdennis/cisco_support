b382a618077740bb7d0e049eee222b7d444f4265
Dennis Roth <rothdennis92@gmail.com>
2024-02-22 13:55:32 +0100

    add proxies to eox

--- src/cisco_support/eox.py
+++ src/cisco_support/eox.py
@@ -2,26 +2,28 @@ import requests
 from . import auth
 
 class EoX:
-    def __init__(self, client_id, client_secret):
+    def __init__(self, client_id, client_secret, proxies=None, verify=True):
         self.access_token = auth.generate_access_token(client_id, client_secret)
         self.base_url = "https://apix.cisco.com/supporttools/eox/rest/5"
         self.headers = {
             'Authorization': f'Bearer {self.access_token}'
         }
+        self.proxies = proxies
+        self.verify = verify
 
     def get_by_dates(self, start_date, end_date, page_index=1):
         url = f"{self.base_url}/EOXByDates/{page_index}/{start_date}/{end_date}?responseencoding=json"
-        response = requests.get(url, headers=self.headers)
+        response = requests.get(url, headers=self.headers, verify=self.verify, proxies=self.proxy)
         return response.json()
 
     def get_by_product_ids(self, product_ids, page_index=1):
         url = f"{self.base_url}/EOXByProductID/{page_index}/{','.join(product_ids)}?responseencoding=json"
-        response = requests.get(url, headers=self.headers)
+        response = requests.get(url, headers=self.headers, verify=self.verify, proxies=self.proxy)
         return response.json()
 
     def get_by_serial_numbers(self, serial_numbers, page_index=1):
         url = f"{self.base_url}/EOXBySerialNumber/{page_index}/{','.join(serial_numbers)}?responseencoding=json"
-        response = requests.get(url, headers=self.headers)
+        response = requests.get(url, headers=self.headers, verify=self.verify, proxies=self.proxy)
         return response.json()
 
     def get_by_software_release_strings(self, softwares, page_index=1):
@@ -29,5 +31,5 @@ class EoX:
         for i, software in enumerate(softwares):
             sw.append(f"input{i+1}={software}")
         url = f"{self.base_url}/EOXBySWReleaseString/{page_index}?{'&'.join(sw)}&responseencoding=json"
-        response = requests.get(url, headers=self.headers)
+        response = requests.get(url, headers=self.headers, verify=self.verify, proxies=self.proxy)
         return response.json()
