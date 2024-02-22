feea750daec87258a02dcd0fc2a5a5cf376e6ed7
James Milner <james.milner@element.com>
2024-02-22 16:44:51 +0000

    Fixed proxies instance var in EoX

--- src/cisco_support/eox.py
+++ src/cisco_support/eox.py
@@ -13,17 +13,17 @@ class EoX:
 
     def get_by_dates(self, start_date, end_date, page_index=1):
         url = f"{self.base_url}/EOXByDates/{page_index}/{start_date}/{end_date}?responseencoding=json"
-        response = requests.get(url, headers=self.headers, verify=self.verify, proxies=self.proxy)
+        response = requests.get(url, headers=self.headers, verify=self.verify, proxies=self.proxies)
         return response.json()
 
     def get_by_product_ids(self, product_ids, page_index=1):
         url = f"{self.base_url}/EOXByProductID/{page_index}/{','.join(product_ids)}?responseencoding=json"
-        response = requests.get(url, headers=self.headers, verify=self.verify, proxies=self.proxy)
+        response = requests.get(url, headers=self.headers, verify=self.verify, proxies=self.proxies)
         return response.json()
 
     def get_by_serial_numbers(self, serial_numbers, page_index=1):
         url = f"{self.base_url}/EOXBySerialNumber/{page_index}/{','.join(serial_numbers)}?responseencoding=json"
-        response = requests.get(url, headers=self.headers, verify=self.verify, proxies=self.proxy)
+        response = requests.get(url, headers=self.headers, verify=self.verify, proxies=self.proxies)
         return response.json()
 
     def get_by_software_release_strings(self, softwares, page_index=1):
@@ -31,5 +31,5 @@ class EoX:
         for i, software in enumerate(softwares):
             sw.append(f"input{i+1}={software}")
         url = f"{self.base_url}/EOXBySWReleaseString/{page_index}?{'&'.join(sw)}&responseencoding=json"
-        response = requests.get(url, headers=self.headers, verify=self.verify, proxies=self.proxy)
+        response = requests.get(url, headers=self.headers, verify=self.verify, proxies=self.proxies)
         return response.json()
